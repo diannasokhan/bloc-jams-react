ed8948371128a2b7d0a2a6cc102f91d45915bb02
Dianna Sokhan <diannasokhan@gmail.com>
2019-03-22 00:27:55 -0400

    adding player bar

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar'
 
 
 class Album extends Component{
@@ -42,6 +43,13 @@ class Album extends Component{
 			this.play();
 		}
 	}
+	handlePrevClick(){
+	const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song)	
+	const newIndex = Math.max(0, currentIndex - 1);
+	const newSong = this.state.album.songs[newIndex];
+	this.setSong(newSong);
+	this.play();
+	}
 	getSongIcon(song, index){
 		if(this.state.isPlaying && this.state.currentSong === song){
 			return (
@@ -65,7 +73,7 @@ class Album extends Component{
 		handleMouseLeave(){
 			this.setState({onHover : false})
 		}
-	render(){
+		render(){
 		return(
 			<section className="album">
 				<section id="album-info">
@@ -96,6 +104,11 @@ class Album extends Component{
 					 )}
 					</tbody>
 				</table>
+				<PlayerBar 
+				isPlaying={this.state.isPlaying} 
+				currentSong={this.state.currentSong}
+				handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+				handlePrevClick={() => this.handlePrevClick()} />
 			</section>
 
 		)
